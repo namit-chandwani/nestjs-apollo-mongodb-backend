23ab4d2c802011f04a928e0c054bdd8e42b904a8
Namit Chandwani <namitchandwani99@gmail.com>
2021-01-12 01:33:00 +0530

    Import data model

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,8 @@ import {
   MongoDBDataProvider,
 } from '@graphback/runtime-mongo';
 import { buildGraphbackAPI } from 'graphback';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 // Mongo Connection URL
 const url = 'mongodb://localhost:27017';
@@ -23,28 +25,11 @@ export const databaseProviders = [
       // Create the MongoDB Data provider creator function
       const dataProviderCreator = createMongoDbProvider(db);
 
-      // The business model
-      const userModel = `
-     """ @model """
-    type Note {
-      _id: GraphbackObjectID!
-      title: String!
-      description: String
-      """
-      @oneToMany(field: 'note')
-      """
-      comments: [Comment]!
-    }
-    
-    """ @model """
-    type Comment {
-      _id: GraphbackObjectID!
-      text: String
-      description: String
-    }
-    
-    scalar GraphbackObjectID
-    `;
+      // Import the data model
+      const userModel = readFileSync(
+        resolve(__dirname, '..', '..', 'model', 'datamodel.graphql'),
+        'utf8',
+      );
 
       return buildGraphbackAPI(userModel, {
         dataProviderCreator,
